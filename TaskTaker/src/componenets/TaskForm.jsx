653505e8bef7f1256f2356eb439cc86053e9a13b
MrPrakashR <77004328+MrPrakashR@users.noreply.github.com>
2024-01-09 14:07:09 +0000

    Task to do react application done

---
+++ TaskTaker/src/componenets/TaskForm.jsx
@@ -0,0 +1,90 @@
+import React , {useState} from "react";
+
+import Tag from "./Tag";
+import "./TaskForm.css";
+
+const TaskForm = ({setTasks}) => {
+
+  const [taskData, setTaskData] = useState({
+    task:"",
+    status:"todo",
+    tags: []
+  })
+
+  const handleChange = (e) => {
+    const { name,value } = e.target
+    setTaskData(prev => {
+        return { ...prev, [name] : value }
+      }
+      )
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTasks(prev=>{
+      return [...prev,taskData]
+    })
+    setTaskData({
+      task:"",
+      status:"todo",
+      tags: []
+    })
+  }
+
+  const checkTag = (tag) => {
+    return taskData.tags.some(item => item == tag)
+  }
+
+  const selectTag = (tag) => {
+    
+    if (taskData.tags.some(item => item == tag)) {
+      const filterTag = taskData.tags.filter(item => item != tag)
+      setTaskData(prev => {
+        return {...prev, tags: filterTag}
+      })
+    } else {
+      setTaskData(prev => {
+        return {...prev, tags: [...prev.tags,tag]}
+      })
+    }
+
+  }
+
+  return (
+    <header className="app_header">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="task"
+          value={taskData.task}
+          className="task_input"
+          placeholder="Enter your task"
+          onChange={handleChange}
+        />
+
+        <div className="task_form_bottom_line">
+          <div>
+            <Tag tagName="HTML" selectTag={selectTag} selected={checkTag("HTML")} />
+            <Tag tagName="CSS" selectTag={selectTag} selected={checkTag("CSS")} />
+            <Tag tagName="JAVASCRIPT" selectTag={selectTag} selected={checkTag("JAVASCRIPT")} />
+            <Tag tagName="React" selectTag={selectTag} selected={checkTag("React")} />
+          </div>
+
+          <div>
+            <select className="task_status" name="status" onChange={handleChange} value={taskData.status}>
+              <option className="todo">To do</option>
+              <option className="doing">Doing</option>
+              <option className="done">Done</option>
+            </select>
+
+            <button type="submit" className="task_submit">+ Add Task</button>
+          </div>
+
+        </div>
+
+      </form>
+    </header>
+    
+  );
+};
+export default TaskForm;
--- first-application/src/componenets/CreateTodo.jsx
+++ first-application/src/componenets/CreateTodo.jsx
@@ -1,21 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 
 
 const CreateTodo = () => {
-  const task = 2
-  const hideButton = false
-  const countTask = () => {
-    return task == 0 ? "No Task" : "Task : " + task
-  }
+
+  let [task,setTask] = useState(0)
+  let [input, setInput] = useState("")
+  const tasks = ["Task 1","Task 2","Task 3"]
+  // const hideButton = false
+  // const countTask = () => {
+  //   return task == 0 ? "No Task" : "Task : " + task
+  // }
   const styles = {
     backgroundColor : "red"
   }
 
+  const handleClick = () => {
+    setTask(task++)
+    console.log("Add Task")
+  }
+
+  const handleChange = (event) => {
+    setInput(event.target.value)
+  }
+
   return (
     <>
-        <h1 style={styles}>{countTask()}</h1> 
-        <button className="btn" disabled={hideButton}>Add Task</button>
-    </>
+        <h1 style={styles}>{task}</h1> 
+        <h1>Input : {input}</h1> 
+        <input type="text" onChange={handleChange} />
+        <button onClick={handleClick} className="btn">Add Task</button>
+        <ul>
+          {
+          tasks.map((element) => (
+            <li key={element}>{element}</li>
+          ))}
+        </ul>
+        </>
   )
 }
 export default CreateTodo
